a429c172a4c18024f81191c8ec5eb9398205d6b5
msseuoni <smashgoonthewell5050@gmail.com>
2023-07-16 09:39:25 +0900

    kadaiyou

---
+++ php02/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>本ブックマーク登録アプリ</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body>
+
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+    <div class="navbar-header"><a class="navbar-brand" href="select.php">本ブックマーク登録アプリ</a></div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<form method="post" action="insert.php">
+  <div class="jumbotron">
+   <fieldset>
+    <legend>好きな本を登録しよう！</legend>
+     <label>書籍名：<input type="text" name="name"></label><br>
+     <label>作者：<input type="text" name="Author"></label><br>
+     <label><textArea name="Comment" rows="4" cols="40"></textArea></label><br>
+     <input type="submit" value="送信">
+    </fieldset>
+  </div>
+</form>
+
+<?php
+//1.  DB接続します
+try {
+  //Password:MAMP='root',XAMPP=''
+  $pdo = new PDO('mysql:dbname=mss_db;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {
+  exit('DB Connection Error'.$e->getMessage());
+}
+
+//２．データ登録SQL作成
+$sql = "SELECT * FROM gs_bm_table;";
+$stmt = $pdo->prepare($sql);
+$status = $stmt->execute();
+
+//３．データ表示
+$view="";
+if($status==false) {
+    //execute（SQL実行時にエラーがある場合）
+  $error = $stmt->errorInfo();
+  exit("SQL_Error:".$error[2]);
+
+}else{
+  //Selectデータの数だけ自動でループしてくれる
+  //FETCH_ASSOC=http://php.net/manual/ja/pdostatement.fetch.php
+  while( $res = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $view .= '<p>';
+    $view .= $res['No'].','.$res['name'].','.$res['Author'].','.$res['Comment'];
+    $view .= '<p>';
+  }
+
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>ブックマーク登録アプリ</title>
+<link rel="stylesheet" href="css/range.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body id="main">
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">ブックマーク済み一覧</a>
+      </div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<div>
+    <div class="container jumbotron"><?=$view?></div>
+</div>
+<!-- Main[End] -->
+
+</body>
+</html>
+
+
+
+<!-- Main[End] -->
+
+
+</body>
+</html>
---
+++ php02/insert.php
@@ -0,0 +1,36 @@
+<?php
+//1. POSTデータ取得
+//$name = filter_input( INPUT_GET, ","name" ); //こういうのもあるよ
+//$email = filter_input( INPUT_POST, "email" ); //こういうのもあるよ
+$name = $_POST['name'];
+$Author = $_POST['Author'];
+$Comment = $_POST['Comment'];
+
+
+//2. DB接続します
+try {
+  //Password:MAMP='root',XAMPP=''
+  $pdo = new PDO('mysql:dbname=mss_db;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {
+  exit('DB Connection Error:'.$e->getMessage());
+}
+
+
+//３．データ登録SQL作成
+$stmt = $pdo->prepare("INSERT INTO `gs_bm_table`(name,Author,Comment,date)VALUES(:name, :Author, :Comment, sysdate());");
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':Author', $Author, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':Comment', $Comment, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("SQL_ERROR:".$error[2]);
+}else{
+  //５．index.phpへリダイレクト
+  header("Location: index.php");
+  exit();
+}
+?>
