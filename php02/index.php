264ef970796ed6e48f281d899f3612f456ae2c60
msseuoni <smashgoonthewell5050@gmail.com>
2023-07-16 21:42:15 +0900

    kadaiyou

--- php02/index.php
+++ php02/index.php
@@ -35,7 +35,7 @@
 //1.  DB接続します
 try {
   //Password:MAMP='root',XAMPP=''
-  $pdo = new PDO('mysql:dbname=mss_db;charset=utf8;host=localhost','root','');
+  new PDO('mysql:dbname=msseuoni_mss_db;charset=utf8;host=mysql57.msseuoni.sakura.ne.jp,msseuoni,11395029iS');
 } catch (PDOException $e) {
   exit('DB Connection Error'.$e->getMessage());
 }
